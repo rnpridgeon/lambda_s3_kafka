ff1f81ea54117a520e0c73819c2a945716f1ba9d
Magnus Edenhill <magnus@edenhill.se>
2019-04-29 20:08:37 +0200

    Use confluent-kafka-python

--- lambda_s3_kafka.py
+++ lambda_s3_kafka.py
@@ -1,11 +1,9 @@
-from __future__ import print_function
-
 import json
 import boto3
-from kafka import KafkaProducer
+from confluent_kafka import Producer
 import urllib
-import ssl
 import logging
+import certifi
 
 root = logging.getLogger()
 if root.handlers:
@@ -17,39 +15,51 @@ print('Loading function')
 
 s3 = boto3.client('s3')
 
-context = ssl.create_default_context()
-context.options &= ssl.OP_NO_TLSv1
-context.options &= ssl.OP_NO_TLSv1_1
-
-producer = KafkaProducer(
-   bootstrap_servers=['pkc-loyje.us-central1.gcp.confluent.cloud:9092'],
-   value_serializer=lambda m: json.dumps(m).encode('ascii'),
-   retry_backoff_ms=500,
-   request_timeout_ms=20000,
-   security_protocol='SASL_SSL',
-   sasl_mechanism='PLAIN',
-   ssl_context=context,
-   sasl_plain_username='KAQ6FBDAGJHXTNUD',
-   sasl_plain_password='+Vz/bZr89unWz8f2ufuDUeJgKSB2/BBFtAsxgCM6cstG2WrO6cK4lMTfoTyewSUv')
+producer = Producer({
+    'bootstrap.servers': 'FIXME',
+    'security.protocol': 'SASL_SSL',
+    'sasl.mechanism': 'PLAIN',
+    'sasl.username': 'FIXME',
+    'sasl.password': 'FIXME',
+    'ssl.ca.location': certifi.where(),
+    'message.timeout.ms': 10*1000})
 
 
+def delivery_report(err, msg, event):
+    """ Delivery report callback is called once per message
+        to indicate delivery success (err is None) or permanent
+        delivery failure (err is not None).
+        The callback is triggered by calling flush() or poll(). """
+    if err is not None:
+        print('Failed to produce event {}: {}'.format(event, err))
+        #raise err
+    else:
+        print('Produced event {} to {} [{}]'.format(event, msg.topic(), msg.partition()))
 
 def lambda_handler(event, context):
-        
+    """ AWS Lambda handler entry point """
     print("Received event: " + json.dumps(event, indent=2))
 
     # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
-    try:
-        print("We have new object. In bucket {}, with key {}".format(bucket, key))
-        future = producer.send("webapp","We have new object. In bucket {}, with key {}".format(bucket, key))
-        record_metadata = future.get(timeout=10)
-        print("sent event to Kafka! topic {} partition {} offset {}".format(record_metadata.topic, record_metadata.partition, record_metadata.offset))
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
 
-    except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        raise e
+    print("We have new object. In bucket {}, with key {}".format(bucket, key))
+
+    # Asynchronous produce
+    producer.produce("webapp",
+                     json.dumps(event).encode('utf-8'),
+                     key=key.encode('utf-8'),
+                     on_delivery=lambda err, msg: delivery_report(err, msg, event))
+
+    # Wait (up to message.timeout.ms) for message to be produced
+    # or fail, delivery status is signalled via the delivery_report method
+    # that is triggered from this flush() call (or poll()).
+    producer.flush()
 
 
+if __name__ == '__main__':
+    # Test
+    lambda_handler({'Records': [ { 's3': {
+        'bucket': {'name': 'testbucket'},
+        'object': {'key': u'testkey'}}} ]}, None)
